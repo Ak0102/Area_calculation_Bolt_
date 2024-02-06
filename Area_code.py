ffb72e8fa996797a81ac19ba8fca0791aabb6683
Ankit Jha <66528749+Ak0102@users.noreply.github.com>
2024-02-06 22:14:02 +0530

    Update Area_code.py

--- Area_code.py
+++ Area_code.py
@@ -5,4 +5,11 @@ b = float(input("Enter smaller Dia: "))
 c = 3.14*a*a/4-3.14*b*b/4
 area = round(c,2)
 print("area of curves : " , area)
-sleep(3)
+sleep(10)
+#for code running in command prompt
+#you have to run some library
+#python -m pip install auto-py-to-exe
+#then after
+#python -m auto_py_to_exe
+#then it will ask for the directory path
+#choose your directory or direct file path.
