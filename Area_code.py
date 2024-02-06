1fc6bad7d4d56ff330cf24a064f236c8884f6b94
Ankit Jha <66528749+Ak0102@users.noreply.github.com>
2024-02-06 22:07:20 +0530

    Add files via upload

---
+++ Area_code.py
@@ -0,0 +1,8 @@
+import time
+from time import sleep
+a = float(input("Enter Bigger Dia: "))
+b = float(input("Enter smaller Dia: "))
+c = 3.14*a*a/4-3.14*b*b/4
+area = round(c,2)
+print("area of curves : " , area)
+sleep(3)
